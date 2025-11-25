33b7a8a35c3fa292a0e7b299a4c8e0c47e0e2bbd
Nikhil Muskur <nikhil6019@icloud.com>
2025-11-25 13:40:01 +0530

    - Initial Commit

---
+++ DummyLib/DummyLib/DummyLib.h
@@ -0,0 +1,16 @@
+//
+//  DummyLib.h
+//  DummyLib
+//
+//  Created by Nikhil Muskur on 25/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyLib : NSObject
+
++ (NSString *)did;
+
++ (NSString *)sid;
+
+@end
---
+++ DummyLib/DummyLib/DummyLib.m
@@ -0,0 +1,26 @@
+//
+//  DummyLib.m
+//  DummyLib
+//
+//  Created by Nikhil Muskur on 25/11/25.
+//
+
+#import "DummyLib.h"
+
+@implementation DummyLib
+
+
+
++ (NSString *)sid {
+    
+    NSString *val = @"Hello";
+    return [val copy];
+}
+
++ (NSString *)did {
+    
+    NSString *val = @"Hello";
+    return [val copy];
+}
+
+@end
