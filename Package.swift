d439e16dfbce0f893651eb537217ac69fabbf3c2
Nikhil Muskur <nikhil6019@icloud.com>
2025-11-25 17:38:49 +0530

    - Updated File Link

--- Package.swift
+++ Package.swift
@@ -10,9 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DummyLib",
-            url: "https://github.com/nik6018/DummyFramework/releases/download/1.1.0/My_Dummy.xcframework.zip",
+            url: "https://github.com/nik6018/DummyFramework/releases/download/ios-1.2.0/My_Dummy.xcframework.zip",
             checksum: "39ae1975ddf7c92b8d6b597afd989655f8e15f305a4d5d2fb0355cb0d8fd09a8"
         )
     ]
 )
-
