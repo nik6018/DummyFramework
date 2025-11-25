2a1addc948bc612d21f51d22f21cbd7a75733987
Nikhil Muskur <nikhil6019@icloud.com>
2025-11-25 13:51:04 +0530

    - Added package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "My_Dummy",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "DummyLib", targets: ["DummyLib"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DummyLib",
+            url: "__BINARY_URL__",
+            checksum: "39ae1975ddf7c92b8d6b597afd989655f8e15f305a4d5d2fb0355cb0d8fd09a8"
+        )
+    ]
+)
+
